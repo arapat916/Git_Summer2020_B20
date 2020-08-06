8ada59ad30dbd5357aeecaed82d57d7d7ab71175
arapat916 <arapat916@gmail.com>
2020-08-05 20:55:53 -0400

    add GitFun.java

---
+++ src/utility/GitFun.java
@@ -0,0 +1,12 @@
+package utility;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
+public class GitFun {
+
+    public static void main(String[] args) {
+
+        System.out.println("Everything is fun");
+    }
+}
